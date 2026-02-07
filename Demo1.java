84dfdd317a80cd6c3b6be3a893734a772d90808e
Dhayanithi-dew <mdhaya81@gmail.com>
2026-02-07 14:23:26 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Demo1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n = sc.nextInt();
+
+        int num = 0;
+        for(int i = 0;i <= 0;i++){
+            if (n > 0){
+                System.out.println("pastive");
+            }else if (n < 0){
+                System.out.println("negative");
+            }else{
+                System.out.println("Zero");
+
+            }
+
+
+        }
+
+    }
+}
